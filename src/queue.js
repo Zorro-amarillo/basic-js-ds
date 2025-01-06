fd4d35de3ff3c369e980ed645b6afcc49156b1cf
Yana Malakhova <ya6ka0@gmail.com>
2025-01-06 19:29:03 +0300

    feat: write queue solution

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.queueArr = []
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.queueArr[0];
+  }
+
+  enqueue(value) {
+    if (this.queueArr.length === 0) {
+      this.queueArr.push(new ListNode(value));
+    } else {
+      checkNextValue(this.queueArr[0]);
+
+      function checkNextValue(obj) {
+        if (obj.next === null) {
+          obj.next = new ListNode(value);
+        } else {
+          checkNextValue(obj.next);
+        }
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const firstItemValue = this.queueArr[0].value;
+    const firstItemNextValue = JSON.parse(JSON.stringify(this.queueArr[0].next));
+
+    this.queueArr = [firstItemNextValue];
+
+    return firstItemValue;
   }
 }
 
